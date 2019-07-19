0b2a44c309b71779e2e69e5e9ad2cc6dba6d7c20
Fazt <fabianncdev@gmail.com>
2019-07-19 16:34:41 +0000

    some comments added

--- src/app.js
+++ src/app.js
@@ -9,17 +9,19 @@ const app = express();
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
-
+// middlewares
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 
+// routes
 require('./routes')(app);
 
+// 404 handler
 app.use((req, res) => {
   res.status(404).render("404");
 });
 
+// starting the app
 http.createServer(app).listen(3000, () => {
   console.log('GuestBook App started on port 3000');
 });
